dc24154dcae3b4e47bb5dd45366b1f56dae07298
Documenter.jl <documenter@juliadocs.github.io>
2022-04-08 12:21:33 +0000

    build based on a5d5933

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PDFEstimators","category":"page"},{"location":"#PDFEstimators","page":"Home","title":"PDFEstimators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PDFEstimators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PDFEstimators]","category":"page"}]
+}
